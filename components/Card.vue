9a0d584929c20af1d1db641e7512372fa664c10b
HicksonWei <hanks199021@gmail.com>
2020-04-02 23:14:53 +0800

    highlight test

--- components/Card.vue
+++ components/Card.vue
@@ -78,95 +78,94 @@ export default {
   /deep/ code {
     word-wrap: break-word;
     white-space: pre-line;
+  }
 
-    /deep/ .hljs {
-      display: block;
-      overflow-x: auto;
-      background: #2b2b2b;
-      color: #f8f8f2;
-      padding: 0.5em;
-    }
+  /deep/ .hljs {
+    display: block;
+    overflow-x: auto;
+    background: #2b2b2b;
+    color: #f8f8f2;
+    padding: 0.5em;
+  }
 
-    /deep/ .hljs-comment,
-    /deep/ .hljs-quote {
-      color: #d4d0ab;
-    }
+  /deep/ .hljs-comment,
+  /deep/ .hljs-quote {
+    color: #d4d0ab;
+  }
 
-    /deep/ .hljs-variable,
-    /deep/ .hljs-template-variable,
-    /deep/ .hljs-tag,
-    /deep/ .hljs-name,
-    /deep/ .hljs-selector-id,
-    /deep/ .hljs-selector-class,
-    /deep/ .hljs-regexp,
-    /deep/ .hljs-hljs-deletion {
-      color: #ffa07a;
-    }
+  /deep/ .hljs-variable,
+  /deep/ .hljs-template-variable,
+  /deep/ .hljs-tag,
+  /deep/ .hljs-name,
+  /deep/ .hljs-selector-id,
+  /deep/ .hljs-selector-class,
+  /deep/ .hljs-regexp,
+  /deep/ .hljs-hljs-deletion {
+    color: #ffa07a;
+  }
 
-    /deep/ .hljs-number,
+  /deep/ .hljs-number,
+  /deep/ .hljs-built_in,
+  /deep/ .hljs-builtin-name,
+  /deep/ .hljs-literal,
+  /deep/ .hljs-type,
+  /deep/ .hljs-params,
+  /deep/ .hljs-meta,
+  /deep/ .hljs-link {
+    color: #ffa07a;
+  }
+
+  /deep/ .hljs-attribute {
+    color: #ffd700;
+  }
+
+  /deep/ .hljs-string,
+  /deep/ .hljs-symbol,
+  /deep/ .hljs-bullet,
+  /deep/ .hljs-addition {
+    color: #abe338;
+  }
+
+  /deep/ .hljs-title,
+  /deep/ .hljs-section {
+    color: #00e0e0;
+  }
+
+  /deep/ .hljs-keyword,
+  /deep/ .hljs-selector-tag {
+    color: #dcc6e0;
+  }
+
+  /deep/ .hljs-emphasis {
+    font-style: italic;
+  }
+
+  /deep/ .hljs-strong {
+    font-weight: bold;
+  }
+
+  @media screen and (-ms-high-contrast: active) {
+    /deep/ .hljs-addition,
+    /deep/ .hljs-attribute,
     /deep/ .hljs-built_in,
     /deep/ .hljs-builtin-name,
+    /deep/ .hljs-bullet,
+    /deep/ .hljs-comment,
+    /deep/ .hljs-link,
     /deep/ .hljs-literal,
-    /deep/ .hljs-type,
-    /deep/ .hljs-params,
     /deep/ .hljs-meta,
-    /deep/ .hljs-link {
-      color: #ffa07a;
-    }
-
-    /deep/ .hljs-attribute {
-      color: #ffd700;
-    }
-
+    /deep/ .hljs-number,
+    /deep/ .hljs-params,
     /deep/ .hljs-string,
     /deep/ .hljs-symbol,
-    /deep/ .hljs-bullet,
-    /deep/ .hljs-addition {
-      color: #abe338;
+    /deep/ .hljs-type,
+    /deep/ .hljs-quote {
+      color: highlight;
     }
 
-    /deep/ .hljs-title,
-    /deep/ .hljs-section {
-      color: #00e0e0;
-    }
-
-    /* Purple */
     /deep/ .hljs-keyword,
     /deep/ .hljs-selector-tag {
-      color: #dcc6e0;
-    }
-
-    /deep/ .hljs-emphasis {
-      font-style: italic;
-    }
-
-    /deep/ .hljs-strong {
       font-weight: bold;
-    }
-
-    @media screen and (-ms-high-contrast: active) {
-      /deep/ .hljs-addition,
-      /deep/ .hljs-attribute,
-      /deep/ .hljs-built_in,
-      /deep/ .hljs-builtin-name,
-      /deep/ .hljs-bullet,
-      /deep/ .hljs-comment,
-      /deep/ .hljs-link,
-      /deep/ .hljs-literal,
-      /deep/ .hljs-meta,
-      /deep/ .hljs-number,
-      /deep/ .hljs-params,
-      /deep/ .hljs-string,
-      /deep/ .hljs-symbol,
-      /deep/ .hljs-type,
-      /deep/ .hljs-quote {
-        color: highlight;
-      }
-
-      /deep/ .hljs-keyword,
-      /deep/ .hljs-selector-tag {
-        font-weight: bold;
-      }
     }
   }
 }
